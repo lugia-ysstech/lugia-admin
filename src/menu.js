6f38a48198f11152a7bb2fdc346672348011b5d3
SleepyAries <410100498@qq.com>
2019-02-27 16:01:18 +0800

    修改    login路由逻辑修改 调整为二级路由    404页面修改 新增    增加login文件    header avatar 增加退出登陆选项

--- src/menu.js
+++ src/menu.js
@@ -7,28 +7,6 @@ import Detail from "./pages/detail";
 import Result from "./pages/result";
 import List from "./pages/list";
 
-const data =[
-  {
-    name:'Dashboard',
-    icon:'lugia-icon-financial_sad',
-    text: "Dashboard",
-    children:[
-      {
-        name:'analyse',
-        icon:'lugia-icon-financial_sad',
-        text: "分析页",
-      },
-      {
-        name: "monitor",
-        text: "监控页",
-        icon:'lugia-icon-financial_sad',
-      },
-    ]
-  },
-  {name:'form', text: "表单页", icon: "lugia-icon-financial_editor"},
-  {name:'list', text: "列表页", icon: "lugia-icon-financial_table" },
-  {name:'detail', text: "详情页", icon: "lugia-icon-financial_sad_o" },
-];
 
 export default [
   {
@@ -37,33 +15,33 @@ export default [
     icon: "lugia-icon-financial_sad",
     children: [
       {
-        value: "/analyse",
+        value: "/pages/analyse",
         text: "分析页",
         path:'./pages/Dashboard/analyse',
       },
       {
-        value: "/monitor",
+        value: "/pages/monitor",
         text: "监控页",
         path:'./pages/Dashboard/monitor',
       },
       {
-        value: "/desk",
+        value: "/pages/desk",
         text: "工作台",
         path:'./pages/Dashboard/desk',
       }
     ]
   },
-  { value: "/user", text: "用户管理", icon: "lugia-icon-financial_user", path:'./pages/user', },
-  { value: "/form", text: "表单页", icon: "lugia-icon-financial_editor", component: Form },
-  { value: "/list", text: "列表页", icon: "lugia-icon-financial_table" ,component: List},
-  { value: "/detail", text: "详情页", icon: "lugia-icon-financial_sad_o" ,component: Detail},
+  { value: "/pages/user", text: "用户管理", icon: "lugia-icon-financial_user", path:'./pages/user', },
+  { value: "/pages/form", text: "表单页", icon: "lugia-icon-financial_editor", component: Form },
+  { value: "/pages/list", text: "列表页", icon: "lugia-icon-financial_table" ,component: List},
+  { value: "/pages/detail", text: "详情页", icon: "lugia-icon-financial_sad_o" ,component: Detail},
   {
-    value: "/result",
+    value: "/pages/result",
     text: "结果页",
     icon: "lugia-icon-reminder_check_circle_o",
     component: Result
   },
-  { value: "/abnormal", text: "异常页", icon: "lugia-icon-reminder_warning" , component: Abnormal},
-  { value: "/personal", text: "个人页", icon: "lugia-icon-financial_user" ,component: Personal}
+  { value: "/pages/abnormal", text: "异常页", icon: "lugia-icon-reminder_warning" , component: Abnormal},
+  { value: "/pages/personal", text: "个人页", icon: "lugia-icon-financial_user" ,component: Personal}
 ];
 
