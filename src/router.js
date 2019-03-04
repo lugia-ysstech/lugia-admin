bb60ce3b04e8da2e9aa75fb4664357a646117c39
jelly@pudding <liang_guodong@126.com>
2019-03-04 11:15:11 +0800

    修改     重构优化

--- src/router.js
+++ src/router.js
@@ -1,54 +1,55 @@
-import { Redirect } from '@lugia/lugiax-router/target/lib';
-import React from 'react';
-import routingConfig from './routing.config';
+import { Redirect } from "@lugia/lugiax-router/target/lib";
+import React from "react";
+import routingConfig from "./routing.config";
 
-const getRouter  = (routingConfig,routes) => {
+const getRouter = (routingConfig, routes) => {
   const rout = routes || {};
-  routingConfig.forEach( item => {
-    const {component,value,path} = item;
-    if(component){
+  routingConfig.forEach(item => {
+    const { component, value, path } = item;
+    if (component) {
       rout[value] = {
         // exact: true,
         component: component
       };
-    }else if(path){
+    } else if (path) {
       rout[value] = {
         // exact: true,
-        render: () => import(`${path}`),
+        render: () => import(`${path}`)
       };
-    }else{
-      const {children} = item;
-      if(children){
-        return getRouter (children,rout);
+    } else {
+      const { children } = item;
+      if (children) {
+        return getRouter(children, rout);
       }
     }
   });
   return rout;
-
 };
-export default  {
-  '/pages': {
-    render:  async () => import('./pages/user'),
-    exact: true,
+export default {
+  "/pages": {
+    render: async () => import("./pages/user"),
+    exact: true
   },
   ...getRouter(routingConfig),
-  '/pages/404': {
-    render: async () => import('./components/404'),
+  "/pages/404": {
+    render: async () => import("./components/404")
   },
-  '/pages/403': {
-    render: async () => import('./components/abnormal/403'),
+  "/pages/403": {
+    render: async () => import("./components/abnormal/403")
   },
-  '/pages/500': {
-    render: async () => import('./components/abnormal/500'),
+  "/pages/500": {
+    render: async () => import("./components/abnormal/500")
   },
-  ROOT: {
+  NotFound: {
     isHidden: true,
     render: async () => {
-      return  () => <Redirect
-        to={{
-          pathname: '/pages',
-        }}
-      />;
-    },
+      return () => (
+        <Redirect
+          to={{
+            pathname: "/pages/404"
+          }}
+        />
+      );
+    }
   }
 };
