4b4f8bc901b0089ed43d87edb4c02384f56e94be
韩京博 <hanjingbo@ysstech.com>
2019-03-21 11:14:04 +0800

    增加配置项 extraMega； 更新依赖； 修改 README.md；

--- config/mega.desktop.js
+++ config/mega.desktop.js
@@ -2,7 +2,7 @@
  * Created Date: Friday, March 8th 2019, 4:21:47 pm
  * Author: hanjingbo@ysstech.com | jingboup@gmail.com
  * -----
- * Last Modified: Thursday, March 14th 2019, 4:22:08 pm
+ * Last Modified: Thursday, March 21st 2019, 11:07:57 am
  * Modified By: hanjingbo <hanjingbo@ysstech.com | jingboup@gmail.com>
  * -----
  * Copyright (c) 2019-present, #Lugia#.
@@ -10,4 +10,11 @@
  * JavaScript will save your soul!
  */
 
-module.exports = {};
+module.exports = {
+  extraMega: {
+    engines: {
+      desktop: "1.x",
+      scaffolding: "1.0.3"
+    }
+  }
+};
