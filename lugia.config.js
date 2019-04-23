e38998b91bee5dea3d70a0b9713eaa84e6b45f74
韩京博 <hanjingbo@ysstech.com>
2019-04-23 18:24:15 +0800

    升级 "@lugia/mega-scripts": "^1.3.27"；增加自定义组件功能；

--- lugia.config.js
+++ lugia.config.js
@@ -1,21 +1,35 @@
-import megaDesktop from "./config/mega.desktop.js";
+import { join } from "path";
+import megaDesktopConfig from "./config/mega.desktop.config.json";
 
 export default {
   disableCSSModules: true,
   cssModulesWithAffix: true,
   entry: "./portal/index.js",
   publicPath: "/",
+  alias: {
+    "@": join(__dirname, "./src")
+  },
   extraBabelPlugins: [
     [
       "import",
       {
         libraryName: "@lugia/lugia-web",
         libraryDirectory: "dist"
-      }
+      },
+      "@lugia/lugia-web"
+    ],
+    [
+      "import",
+      {
+        libraryName: "@/components",
+        libraryDirectory: "",
+        camel2DashComponentName: false
+      },
+      "@/components"
     ]
   ],
   applyWebpack(webpackConfig, { webpack, merge }) {
     return webpackConfig;
   },
-  ...megaDesktop
+  ...megaDesktopConfig
 };
