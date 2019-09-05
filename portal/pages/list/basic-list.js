ada873465cf7d791ccf080b459663f98c9ad196b
grguang <945455382@qq.com>
2019-09-05 13:33:21 +0800

    Add:   * list Component;

---
+++ portal/pages/list/basic-list.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * create by grguang
+ *
+ *
+ */
+import React, { Component } from "react";
+import { Input } from "@lugia/lugia-web";
+
+export default class BasicList extends Component {
+  render() {
+    return <div>BasicList</div>;
+  }
+}
---
+++ portal/pages/list/card-list.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * create by grguang
+ *
+ *
+ */
+import React, { Component } from "react";
+import { Input } from "@lugia/lugia-web";
+import BasicList from "./basic-list";
+
+export default class CardList extends Component {
+  render() {
+    return <div>CardList</div>;
+  }
+}
