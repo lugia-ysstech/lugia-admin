cdd8393ea2d9a64f8786aad0432dbade76a4d3f2
grguang <945455382@qq.com>
2019-09-05 13:33:21 +0800

    Add:   * list Component;

---
+++ portal/pages/list/table-list.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * create by grguang
+ *
+ *
+ */
+import React, { Component } from "react";
+import styled from "styled-components";
+import Contetn from "../../components/content";
+import PageHeader from "../../components/page-header";
+import PageContent from "../../components/page-content";
+import TableForm from "../../components/table-list-form";
+const routes = [
+  {
+    path: "/Dashboard/analyse",
+    title: "首页"
+  },
+  {
+    title: "列表页"
+  },
+  {
+    title: "查询表格"
+  }
+];
+
+export default class TableList extends Component {
+  render() {
+    return (
+      <Contetn>
+        <PageHeader routes={routes} title={"查询表格"} />
+        <PageContent>
+          <TableForm />
+        </PageContent>
+      </Contetn>
+    );
+  }
+}
