5ea0df1d10f60f60bd0ac2e616da0f0cf3b66261
grguang <945455382@qq.com>
2019-09-05 13:33:49 +0800

    Add:   * page-header/content Component;

---
+++ portal/components/page-header/index.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * create by grguang
+ *
+ *
+ */
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Breadcrumb } from "@lugia/lugia-web";
+
+const Wrap = styled.div`
+  margin: 8px 0 20px 8px;
+  background: #fff;
+  padding: 16px 20px;
+`;
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.4;
+  margin-bottom: 10px;
+`;
+const Description = styled.div`
+  font-size: 14px;
+  line-height: 1.4;
+`;
+
+export default class PageHeader extends Component {
+  render() {
+    const { routes = [], title, desc } = this.props;
+    return (
+      <Wrap>
+        <Breadcrumb routes={routes} />
+        {title && <Title>{title}</Title>}
+        {desc && <Description>{desc}</Description>}
+      </Wrap>
+    );
+  }
+}
