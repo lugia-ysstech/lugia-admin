0aa8f1acf53232b569611b2104a44976fedd4ef3
jelly@pudding <liang_guodong@126.com>
2019-09-27 17:48:24 +0800

    add    form page.

--- portal/pages/form/basic.js
+++ portal/pages/form/basic.js
@@ -1,13 +1,245 @@
-/**
- *
- * create by grguang
- *
- * @flow
- */
 import React, { Component } from "react";
+import Content from "../../components/content";
+import PageHeader from "../../components/page-header";
+import PageContent from "../../components/page-content";
+import Widget from "@lugia/lugia-web/dist/consts";
+import styled from "styled-components";
+import {
+  Theme,
+  Tabs,
+  Avatar,
+  Input,
+  Button,
+  Select,
+  Icon,
+  DatePicker,
+  NumberInput,
+  Radio
+} from "@lugia/lugia-web";
 
-export default class Demo extends Component{
+const RadioGroup = Radio.Group;
+const RadioButton = Radio.Button;
+
+const { RangePicker } = DatePicker;
+
+const ItemContainer = styled.div`
+  position: relative;
+  height: auto;
+  zoom: 1;
+  display: block;
+  box-sizing: border-box;
+  padding: 12px 0;
+`;
+
+const ItemInnerContainer = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  width: 40%;
+  text-align: right;
+  margin-right: 5px;
+`;
+const ItemInputContainer = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  width: 40%;
+  margin-left: 5px;
+`;
+const TitleContainer = styled.label`
+  position: relative;
+  height: 25px;
+  line-height: 25px;
+  display: inline-block;
+`;
+const TitleText = styled.span`
+  position: relative;
+  display: inline-block;
+  height: 25px;
+  line-height: 25px;
+`;
+const IsSelectText = styled.span`
+  position: relative;
+  display: inline-block;
+  height: 25px;
+  line-height: 25px;
+  color: gray;
+`;
+
+const routes = [
+  {
+    path: "/dashboard/analyse",
+    title: "首页"
+  },
+  {
+    title: "表单页"
+  },
+  {
+    path: "/pages/form/basic",
+    title: "基础表单"
+  }
+];
+const titleData = [
+  {
+    title: "标题:",
+    placeholder: "给目标给个名字"
+  }
+];
+const descData = [
+  {
+    title: "目标描述:",
+    placeholder: "请输入你的阶段性工作目标"
+  }
+];
+const referenceData = [
+  {
+    title: "衡量标准",
+    placeholder: "请输入衡量标准"
+  }
+];
+const visitorData = [
+  {
+    title: "客户",
+    icon: "",
+    isSelect: true,
+    placeholder: "请描述你服务的客户，内部客户直接 @姓名／工号"
+  }
+];
+const inviterData = [
+  {
+    title: "邀评人",
+    isSelect: true,
+    placeholder: "请直接 @姓名／工号，最多可邀请 5 人"
+  }
+];
+const RadioGroupData = [
+  { text: "公开", value: "1" },
+  { text: "部分公开", value: "2" },
+  { text: "不公开", value: "3" }
+];
+const numberInputView = {
+  [Widget.NumberInput]: {
+    Container: {
+      normal: {
+        width: 100
+      }
+    }
+  }
+};
+const inputView = {
+  [Widget.Input]: {
+    Container: {
+      normal: {
+        width: 410
+      }
+    }
+  }
+};
+const buttonView = {
+  [Widget.Button]: {
+    Container: {
+      normal: {
+        margin: {
+          right: 10
+        }
+      }
+    }
+  }
+};
+
+const getDataItem = (
+  <ItemContainer>
+    <ItemInnerContainer>
+      <TitleContainer>
+        <TitleText>{"起止日期"}</TitleText>
+      </TitleContainer>
+    </ItemInnerContainer>
+    <ItemInputContainer>
+      <RangePicker format={"YYYY-MM-DD"} />
+    </ItemInputContainer>
+  </ItemContainer>
+);
+const getWeight = (
+  <ItemContainer>
+    <ItemInnerContainer>
+      <TitleContainer>
+        <TitleText>{"权重"}</TitleText>
+        <IsSelectText>{"(选填)"}</IsSelectText>
+      </TitleContainer>
+    </ItemInnerContainer>
+    <ItemInputContainer>
+      <Theme config={numberInputView}>
+        <NumberInput />
+      </Theme>
+      <TitleText>{"%"}</TitleText>
+    </ItemInputContainer>
+  </ItemContainer>
+);
+const getOperation = (
+  <ItemContainer>
+    <ItemInputContainer />
+    <Theme config={buttonView}>
+      <Button type={"primary"}>{"提交"}</Button>
+      <Button>{"保存"}</Button>
+    </Theme>
+  </ItemContainer>
+);
+const getRadioGroup = (
+  <ItemContainer>
+    <ItemInnerContainer />
+    <ItemInputContainer>
+      <RadioGroup defaultValue="1" valueField="value" data={RadioGroupData} />
+    </ItemInputContainer>
+  </ItemContainer>
+);
+
+const getItem = data => () => {
+  return data.map(item => {
+    const { title, placeholder, icon, isSelect } = item;
+    const leftIcon = icon ? (
+      <div>
+        <Icon icon={icon} />
+      </div>
+    ) : null;
+    return (
+      <ItemContainer>
+        <ItemInnerContainer>
+          <TitleContainer>
+            <TitleText>{title}</TitleText>
+            {isSelect && <IsSelectText>{"(选填)"}</IsSelectText>}
+          </TitleContainer>
+        </ItemInnerContainer>
+        {leftIcon}
+        <ItemInputContainer>
+          <Theme config={inputView}>
+            <Input placeholder={placeholder} />
+          </Theme>
+        </ItemInputContainer>
+      </ItemContainer>
+    );
+  });
+};
+export default class TableList extends Component {
   render() {
-    return <div>form</div>;
+    return (
+      <Content>
+        <PageHeader
+          routes={routes}
+          title={"基础表单"}
+          desc={
+            "表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。"
+          }
+        />
+        <PageContent>
+            {getItem(titleData)()}
+            {getDataItem}
+            {getItem(descData)()}
+            {getItem(referenceData)()}
+            {getItem(visitorData)()}
+            {getItem(inviterData)()}
+            {getWeight}
+            {getRadioGroup}
+            {getOperation}
+        </PageContent>
+      </Content>
+    );
   }
 }
--- portal/pages/form/step.js
+++ portal/pages/form/step.js
@@ -5,9 +5,232 @@
  * @flow
  */
 import React, { Component } from "react";
+import Content from "../../components/content";
+import PageHeader from "../../components/page-header";
+import PageContent from "../../components/page-content";
 
-export default class Demo extends Component{
+import {
+  Theme,
+  Input,
+  Button,
+  Select,
+  Icon,
+  NumberInput,
+  Steps,
+  AmountInput,
+  Avatar
+} from "@lugia/lugia-web";
+import styled from "styled-components";
+import Widget from "@lugia/lugia-web/dist/consts";
+
+const ItemContainer = styled.div`
+  position: relative;
+  height: auto;
+  zoom: 1;
+  display: block;
+  box-sizing: border-box;
+  padding: 12px 0;
+  margin: 0 0 24px;
+`;
+const StepContainer = styled(ItemContainer)`
+  text-align: center;
+  margin: 16px 0;
+`;
+
+const ItemInnerContainer = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  width: 40%;
+  text-align: right;
+  margin-right: 5px;
+`;
+
+const ItemInputContainer = styled.div`
+  display: inline-block;
+  box-sizing: border-box;
+  width: 50%;
+  margin-left: 5px;
+`;
+const TitleContainer = styled.label`
+  position: relative;
+  height: 25px;
+  line-height: 25px;
+  display: inline-block;
+`;
+const TitleText = styled.span`
+  position: relative;
+  display: inline-block;
+  height: 25px;
+  line-height: 25px;
+`;
+const SelectContainer = styled.div`
+  vertical-align: bottom;
+  display: inline-block;
+`;
+const routes = [
+  {
+    path: "/dashboard/analyse",
+    title: "首页"
+  },
+  {
+    title: "表单页"
+  },
+  {
+    path: "/pages/form/step",
+    title: "分步3表单"
+  }
+];
+const inputView = {
+  [Widget.Input]: {
+    Container: {
+      normal: {
+        width: 300
+      }
+    }
+  }
+};
+const getItem = data => () => {
+  return data.map(item => {
+    const {
+      title,
+      inputPlaceholder,
+      isAmount,
+      isSelect,
+      selectData,
+      selectView,
+      selectDefaultValue,
+      inputDefaultValue
+    } = item;
+    return (
+      <ItemContainer>
+        <ItemInnerContainer>
+          <TitleContainer>
+            <TitleText>{title}</TitleText>
+          </TitleContainer>
+        </ItemInnerContainer>
+        <ItemInputContainer>
+          {isSelect && (
+            <Theme config={selectView}>
+              <SelectContainer>
+                <Select createPortal data={selectData} displayField={"label"}  defaultValue={selectDefaultValue}/>
+              </SelectContainer>
+            </Theme>
+          )}
+          <Theme config={inputView}>
+            {!isAmount &&
+              inputPlaceholder && <Input placeholder={inputPlaceholder} />}
+          </Theme>
+          {isAmount && <AmountInput  defaultValue={inputDefaultValue}/>}
+        </ItemInputContainer>
+      </ItemContainer>
+    );
+  });
+};
+
+const steps = [
+  {
+    title: "填写转账信息"
+  },
+  {
+    title: "确认转账信息"
+  },
+  {
+    title: "完成"
+  }
+];
+const getSteps = currentStepNumber => () => {
+  return (
+    <StepContainer>
+      <Steps data={steps} currentStepNumber={currentStepNumber} />
+    </StepContainer>
+  );
+};
+const payData = [
+  {
+    title: "付款账户",
+    isSelect: true,
+    selectData: [{ value: "lugia-design@ysstech.com", label: "lugia-design@ysstech.com" }],
+    selectDefaultValue:"lugia-design@ysstech.com",
+    selectView: {
+      [Widget.Select]: {
+        InputTag: {
+          InputTagWrap: {
+            normal: {
+              width: 300
+            }
+          }
+        }
+      }
+    }
+  }
+];
+const receiptData = [
+  {
+    title: "收款人姓名:",
+    inputPlaceholder: "Lugia"
+  }
+];
+
+const receiptAccountData = [
+  {
+    title: "收款账户:",
+    isSelect: true,
+    selectDefaultValue:"支付宝",
+    selectData: [
+      { value: "支付宝", label: "支付宝" },
+      { value: "银行账户", label: "银行账户" }
+    ],
+    selectView: {
+      [Widget.Select]: {
+        InputTag: {
+          InputTagWrap: {
+            normal: {
+              width: 100
+            }
+          }
+        }
+      }
+    },
+    inputPlaceholder: "lugia-design@ysstech.com"
+  }
+];
+const transferData = [
+  {
+    title: "转账金额:",
+    inputDefaultValue:500,
+    isAmount: true,
+    inputPlaceholder: "请输入衡量标准"
+  }
+];
+const getOperation = (
+  <ItemContainer>
+    <ItemInnerContainer />
+    <ItemInputContainer>
+      <Button type={"primary"}>{"下一步"}</Button>
+    </ItemInputContainer>
+  </ItemContainer>
+);
+
+export default class Demo extends Component {
   render() {
-    return <div>form</div>;
+    return (
+      <Content>
+        <PageHeader
+          routes={routes}
+          title={"分布表单"}
+          desc={"将一个冗长或用户不熟悉的表单任务分成多个步骤，指导用户完成。"}
+        />
+        <PageContent>
+          <div>
+            {getSteps(1)()}
+            {getItem(payData)()}
+            {getItem(receiptAccountData)()}
+            {getItem(receiptData)()}
+            {getItem(transferData)()}
+            {getOperation}
+          </div>
+        </PageContent>
+      </Content>
+    );
   }
 }
