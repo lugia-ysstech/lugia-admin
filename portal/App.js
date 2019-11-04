7682fbb34b2c981376a3a4c946a94dcd0ac5d685
SleepyAries <410100498@qq.com>
2019-11-04 17:09:48 +0800

    update:    fix NavMenu  reRender

--- portal/App.js
+++ portal/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import { createRoute } from "@lugia/lugiax-router";
-import MenuList from "./components/menulist";
+
 import styled from "styled-components";
 import Pages from "./pages";
-import register from "./models/register";
 
 export const firstRouter = {
   "/register/register": {
@@ -23,16 +22,6 @@ export const firstRouter = {
     component: Pages
   }
 };
-
-const ContentContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  overflow: auto;
-  height: 100%;
-  width: 100%;
-  background: #f5f5f9;
-`;
 
 export default () => {
   return (
