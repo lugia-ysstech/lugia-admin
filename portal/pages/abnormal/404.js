418d6b46e13d8e7556aaa6ab834dc266270daf77
Shine_Lee <wangyi_lixian@163.com>
2019-12-06 12:14:46 +0800

    修改一场也中不居中的问题

--- portal/pages/abnormal/404.js
+++ portal/pages/abnormal/404.js
@@ -5,12 +5,26 @@
  * @flow
  */
 import React from "react";
+import styled from "styled-components";
 import Abnormal from '../../components/abnormal/404'
+
+const PageWrapper = styled.div`
+  width: 100%,
+  height: 100%
+  border: 1px solid red;
+  display: flex;
+  align-items: center;
+  >div{ 
+    padding: 0
+  }
+`;
 
 export default class Pages extends React.Component {
   render(){
     return (
-      <Abnormal />
+      <PageWrapper>
+        <Abnormal />
+      </PageWrapper>
     );
 
   }
