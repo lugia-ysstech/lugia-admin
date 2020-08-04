3bf0facbca06213bf9bc92a47302798bcf0c8a76
szfeng <360998950@qq.com>
2020-08-04 18:24:55 +0800

    修改：1、增加outRouting.config.json文件，用来记录外链路由的信息      2、删除demo中的外链信息

--- src/config/router/outRouting.config.js
+++ src/config/router/outRouting.config.js
@@ -1,18 +1,3 @@
 import Iframe from '../../portal/components/iframe';
 
-export default [
-  {
-    id: 'innerBaidu',
-    value: '/www.baidu.com',
-    text: '内部跳转百度',
-    innerLink: true,
-    component: Iframe,
-  },
-  {
-    id: 'outBaidu',
-    value: '/lugia.tech',
-    text: '跳转到Lugia官网',
-    innerLink: false,
-    component: Iframe,
-  },
-];
+export default [];
